ba73045cc9869bc8ec7c5b3183c8ba405e3b55dd
Thomas <75882278+inwuerde@users.noreply.github.com>
2026-07-20 13:26:44 +0200

    Include Google Analytics usage and cookie policy
    Added Google Analytics information and cookie usage details.

--- app/src/pages/Legal.tsx
+++ app/src/pages/Legal.tsx
@@ -112,6 +112,13 @@ export function Datenschutz() {
           </li>
           <li>Der Zeitpunkt der Anfrage.</li>
         </ul>
+        <p>
+                    <strong>Google Analytics</strong>
+Diese Website nutzt den Dienst „Google Analytics“, welcher von der Google Inc. (1600 Amphitheatre Parkway Mountain View, CA 94043, USA) angeboten wird, zur Analyse der Websitebenutzung durch Nutzer. Der Dienst verwendet „Cookies“ – Textdateien, welche auf Ihrem Endgerät gespeichert werden. Die durch die Cookies gesammelten Informationen werden im Regelfall an einen Google-Server in den USA gesandt und dort gespeichert.
+Auf dieser Website greift die IP-Anonymisierung. Die IP-Adresse der Nutzer wird innerhalb der Mitgliedsstaaten der EU und des Europäischen Wirtschaftsraum gekürzt. Durch diese Kürzung entfällt der Personenbezug Ihrer IP-Adresse. Im Rahmen der Vereinbarung zur Auftragsdatenvereinbarung, welche die Websitebetreiber mit der Google Inc. geschlossen haben, erstellt diese mithilfe der gesammelten Informationen eine Auswertung der Websitenutzung und der Websiteaktivität und erbringt mit der Internetnutzung verbundene Dienstleistungen.
+Sie haben die Möglichkeit, die Speicherung des Cookies auf Ihrem Gerät zu verhindern, indem Sie in Ihrem Browser entsprechende Einstellungen vornehmen. Es ist nicht gewährleistet, dass Sie auf alle Funktionen dieser Website ohne Einschränkungen zugreifen können, wenn Ihr Browser keine Cookies zulässt.
+Weiterhin können Sie durch ein Browser-Plugin verhindern, dass die durch Cookies gesammelten Informationen (inklusive Ihrer IP-Adresse) an die Google Inc. gesendet und von der Google Inc. genutzt werden. Folgender Link führt Sie zu dem entsprechenden Plugin: <a href=“https://tools.google.com/dlpage/gaoptout?hl=de“> https://tools.google.com/dlpage/gaoptout?hl=de</a>
+        </p>
         <p className="mt-6 text-sm text-warm-500">
           Der Text ist von Jan Schallaböck und lizenziert unter Creative Commons
           Attribution 4.0 International License.
