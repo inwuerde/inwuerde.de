ad8f79d1e43ae41ce6f0f3d103bda929cfef0b76
Thomas Richter <thomas.karl.richter@gmail.com>
2026-07-29 13:27:58 +0200

    codepath attribute in vite.config entfernt

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,10 +7,9 @@ export default function App() {
   return (<>
     <CookieConsentBanner />
     <Routes>
-       <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/impressum" element={<Impressum />} />
       <Route path="/datenschutz" element={<Datenschutz />} />
     </Routes>
-    </>
-  );
+    </>);
 }
